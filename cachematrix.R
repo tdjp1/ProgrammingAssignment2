ba3a0d2685b0c11f9be9692192777d14e7499c4f
Tim Perkins <tim@theperkins.se>
2014-06-05 10:17:34 +0200

    Added name

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,8 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
+# Tim Perkins 140605
+
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
